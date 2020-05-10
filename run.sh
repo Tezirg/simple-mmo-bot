3170cd1ccd1d5114796c093ab985a602344d4b73
Tezirg <jean.grizet@gmail.com>
2020-05-10 09:42:47 +0200

    Chrome no check default browser

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-google-chrome --disable-web-security --user-data-dir="/tmp/simple-mmo-bot" --disable-xss-auditor --disable-site-isolation-trials ./index.html
+google-chrome --disable-web-security --user-data-dir="/tmp/simple-mmo-bot" --disable-xss-auditor --no-default-browser-check --disable-site-isolation-trials ./index.html
