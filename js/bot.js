8a46247a741eb7c8cc2b2fd44a60812e3bb1420b
Tezirg <jean.grizet@gmail.com>
2020-05-10 17:01:56 +0200

    Auto assign char points

--- js/bot.js
+++ js/bot.js
@@ -1,17 +1,26 @@
 var bot_target_id = "#bot-target";
 var bot_status_id = "#bot-status";
+
 var bot_auto_travel_id = "#auto-travel";
 var bot_auto_travel_combat_id = "#auto-travel-combat";
+
 var bot_auto_quest_id = "#auto-quest";
 var bot_auto_quest_index_id = "#quest-id";
+
 var bot_auto_job_id = "#auto-job";
 var bot_auto_job_num_id = "#job-num";
+
 var bot_auto_battle_arena_id = "#auto-battle-arena";
 var bot_auto_combat_health_id = "#auto-combat-health";
 var bot_auto_combat_use_id = "#auto-combat-use";
+
 var bot_auto_bank_id = "#auto-deposit";
 var bot_auto_bank_keep_id = "#keep-amount";
 var bot_auto_bank_deposit_id = "#deposit-amount";
+
+var bot_auto_points_id = "#auto-points"
+var bot_auto_points_index_id = "#points-index";
+
 var bot_run_bt_id = "#bot-run";
 var bot_stop_bt_id = "#bot-stop";
 var bot_refresh_freq = 1750;
@@ -23,6 +32,7 @@ class SimpleMMOBot {
 	// Character
 	this.user = new BotUser(this.targetID);
 	this.userInterval = null;
+	this.autoPoints = false;
 	// Quests
 	this.autoQuest = false;
 	this.autoQuestID = -1;
@@ -48,6 +58,10 @@ class SimpleMMOBot {
 	    return true;
 	if (this.bank.isBanking)
 	    return true;
+	if (this.user.assignPoint)
+	    return true;
+
+	return false;
     }
     
     start() {
@@ -57,22 +71,31 @@ class SimpleMMOBot {
 	var autoTravelCombat = $(bot_auto_travel_combat_id).is(":checked");
 	this.autoTravel = $(bot_auto_travel_id).is(":checked");
 	this.travel.setTravelCombat(autoTravelCombat);
+	
 	this.autoQuest = $(bot_auto_quest_id).is(":checked");
 	this.autoQuestID = parseInt($(bot_auto_quest_index_id).val());
+	
 	this.autoJob = $(bot_auto_job_id).is(":checked");
 	var jobNumber = parseInt($(bot_auto_job_num_id).val());
 	this.job.setNumber(jobNumber);
+	
 	this.autoBattleArena = $(bot_auto_battle_arena_id).is(":checked");
 	var autoCombatHealth = parseInt($(bot_auto_combat_health_id).val());
 	var autoCombatUse = parseInt($(bot_auto_combat_use_id).val());
 	this.combat.setMinHealth(autoCombatHealth);
 	this.combat.setUseItemHealth(autoCombatUse);
+	
 	this.autoBank = $(bot_auto_bank_id).is(":checked");
 	var bankKeep = parseInt($(bot_auto_bank_keep_id).val());
 	var bankDeposit = parseInt($(bot_auto_bank_deposit_id).val());
 	this.bank.setKeepAmount(bankKeep);
 	this.bank.setDepositAmount(bankDeposit);
 
+	this.autoPoints = $(bot_auto_points_id).is(":checked");
+	var pointIdx = parseInt($(bot_auto_points_index_id).val());
+	this.user.pointsIndex = pointIdx;
+
+	
 	$(this.statusID).text("Running");
 	// Continuously update user values
 	this.userInterval = setInterval(function() {
@@ -80,7 +103,10 @@ class SimpleMMOBot {
 
 	    if (!that.isBusy())
 	    {
-		if (that.autoBank && that.bank.canBank()) {
+		if (that.autoPoints && that.user.level_up) {
+		    that.user.charPoints();
+		}
+		else if (that.autoBank && that.bank.canBank()) {
 		    that.bank.bank();
 		}
 		else if (that.autoQuest && that.quest.canQuest()) {
--- js/user.js
+++ js/user.js
@@ -1,3 +1,5 @@
+var bot_home_url = "https://web.simple-mmo.com/"
+
 var bot_user_current_health_id = "#current_health"
 var bot_user_max_health_id = "#max_health"
 var bot_user_current_xp_id = "#current_exp"
@@ -8,19 +10,32 @@ var bot_user_current_gold_id = "#current_gold"
 var bot_user_current_steps_id = "#current_steps"
 var bot_user_current_quests_id = "#current_quest_points"
 
+var bot_char_url = "https://web.simple-mmo.com/character"
+var bot_char_points_id = "#available_points"
+var bot_char_assign_id = "a:contains('Increase')"
+var bot_char_assign_nb_id = "input[type='number']"
+var bot_char_assign_confirm_id = "button:contains('Add stat points')"
+var bot_char_delay = 855;
+
 class BotUser {
     constructor(targetID) {
 	this.targetID = targetID;
 	this.current_health = -1;
 	this.max_health = -1;
-	this.current_xp = -1;
-	this.ratio_health = -1.0;
+	this.percent_health = -1.0;
+	this.current_xp = 0;
 	this.max_xp = -1;
+	this.level_up = false;
 	this.current_energy = -1;
 	this.max_energy = -1;
 	this.gold = -1;
 	this.steps = -1;
 	this.quests = -1;
+
+	// Char points
+	this.pointsIndex = 0;
+	this.assignPoint = false;
+	this.points = 0;
     }
 
     updateFromID(id_selector) {
@@ -35,13 +50,121 @@ class BotUser {
 	this.current_health = this.updateFromID(bot_user_current_health_id);
 	this.max_health = this.updateFromID(bot_user_max_health_id);
 	this.percent_health = (this.current_health / this.max_health) * 100.0;
+	var last_xp = this.current_xp;
 	this.current_xp = this.updateFromID(bot_user_current_xp_id);
+	if (last_xp > this.current_xp)
+	    this.level_up = true;
 	this.max_xp = this.updateFromID(bot_user_max_xp_id);
 	this.current_energy = this.updateFromID(bot_user_current_energy_id);
 	this.max_energy = this.updateFromID(bot_user_max_energy_id);
 	this.gold = this.updateFromID(bot_user_current_gold_id);
 	this.steps = this.updateFromID(bot_user_current_steps_id)
 	this.quests = this.updateFromID(bot_user_current_quests_id)
+    }
+
+    updateCharPoints() {
+	var that = this;
+	var updated=  new Promise((res, rej) => {
+	    that.points = that.updateFromID(bot_char_points_id);
+	    if (that.points > 0) {
+		res(true);
+	    }
+	    else
+		rej(false);
+	});
+	return updated;
+    }
+
+    selectCharPoints() {
+	var that = this;
+	var selected = new Promise((res, rej) => {
+	    try {
+		var targetDOM = $(that.targetID).contents();
+		var assignBts = targetDOM.find(bot_char_assign_id);
+		setTimeout(function() {
+		    assignBts[that.pointsIndex].click();
+		    res(true);
+		}, bot_char_delay);
+	    }
+	    catch (e) {
+		rej(false);
+	    }
+	});
+	return selected;
+    }
+
+    setNumPoints() {
+	var that = this;
+	var setnum = new Promise((res, rej) => {
+	    var targetDOM = $(that.targetID).contents();
+	    var setInput = targetDOM.find(bot_char_assign_nb_id);
+	    try {
+		setInput.val(that.points);
+		setTimeout(function() {
+		    res(true);
+		}, bot_char_delay);
+	    }
+	    catch {
+		rej(false);
+	    }
+	});
+	return setnum;
+    }
+
+    confirmPoints() {
+	var that = this;
+	var confirm = new Promise((res, rej) => {
+	    var targetDOM = $(that.targetID).contents();
+	    console.log(targetDOM);
+	    var confirmBt = targetDOM.find(bot_char_assign_confirm_id);
+	    console.log(confirmBt);
+	    try {
+		confirmBt[0].click();
+		setTimeout(function() {
+		    res(true);
+		}, bot_char_delay);
+	    }
+	    catch {
+		rej(false);
+	    }
+	});
+	return confirm;
+    }
+
+    triggerAssignPoints() {
+	var that = this;
+	that.updateCharPoints()
+	    .then(function(s) {
+		return that.selectCharPoints();
+	    })
+	    .then(function(s) {
+		return that.setNumPoints();
+	    })
+	    .then(function(s) {
+		return that.confirmPoints();
+	    })
+	    .catch(function(s) {
+		console.log("Assign points failed");
+	    })
+	    .finally(function() {
+		that.points = 0;
+		that.level_up = false;
+		that.assignPoint = false;
+		// Navigate to home
+		$(that.targetID).prop('src', bot_home_url);
+	    });
+    }
+
+    charPoints() {
+	var that = this;
+	that.assignPoint = true;
+	// Handle char loaded
+	$(this.targetID).bind("load", function() {
+	    $(that.targetID).unbind();
+	    that.triggerAssignPoints();
+	});
+	// Navigate to char
+	$(this.targetID).prop('src', bot_char_url);
     }
 
     print() {
